79ef6705f1deeac6ddbf5bfb64a0f34481afaeb8
José Nieto <jose@xiam.io>
2024-04-28 07:41:03 -0600

    remove unused method

--- scanner.go
+++ scanner.go
@@ -16,16 +16,3 @@ const RuneEOF = -1
 func IsEOF(r rune) bool {
 	return r == RuneEOF
 }
-
-func (s State) String() string {
-	switch s {
-	case StateAccept:
-		return "ACCEPT"
-	case StateReject:
-		return "REJECT"
-	case StateContinue:
-		return "CONTINUE"
-	}
-
-	panic("unreachable")
-}
